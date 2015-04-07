a65359fc9be3572db4fe4c05557f1ad3d5a074cb
Ricky <yfb13226>
2015-04-07 15:27:53 +0100

    Added navigate.js

---
+++ navigate.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*jslint node: true, browser: true */
+"use strict";
+function openRegister() {
+   window.location = "https://devweb2014.cis.strath.ac.uk/~yfb13226/CS317/AppCity/register.html";
+}
+
