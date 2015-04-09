2958802058040455b6df01c8df645b6aba8f2114
Smyth4 <ckb12185@uni.strath.ac.uk>
2015-04-09 13:21:06 +0100

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+// Connect to Database Server
+// Stewarts
+$connection = mysqli_connect("devweb2014.cis.strath.ac.uk", "ypb12171", "thouthri", "ypb12171") or die("MySQL Error: " . mysql_error());
+
+// Choose Database
+// Stewart
+mysqli_select_db($connection, "ypb12171") or die("MySQL Error: " . mysql_error());
+
+?>
