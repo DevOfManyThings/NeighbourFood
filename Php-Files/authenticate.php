47fbaa6ad33a7c75f7817968b69d73fbf9c52c73
Smyth4 <ckb12185@uni.strath.ac.uk>
2015-04-14 12:52:17 +0100

    Update authenticate.php

--- Php-Files/authenticate.php
+++ Php-Files/authenticate.php
@@ -9,60 +9,46 @@ $sql = "SELECT Password FROM Client_Details WHERE Email = '$email'";
 
 $result = mysqli_query($connection, $sql);
 
-if (!$result) 
-{
+if (!$result) {
     die('Query Failed: ' . mysql_error());
-}
-else
-{
+} else {
     $pass = "";
-    while ($row = mysqli_fetch_array($result)) 
-    {
+    while ($row = mysqli_fetch_array($result)) {
         $pass = $row["Password"];
-    }  
-}
-        
-if ($password == $pass)
-{
-        // Check if its a Business or Charity that just logged in.
-        $sql = "SELECT Type FROM Client_Details WHERE Email = '$email'";
-        $result = mysqli_query($connection, $sql);
-        
-        $numRows = mysqli_num_rows($result); 
-        
-        $type = "";
-        while ($row = mysqli_fetch_array($result)) 
-        {
-            $type = $row["Type"];
-        }  
-        
-        if($type == "Business")
-        {
-            session_start();
-            $_SESSION['login'] = "business";
-            header('Location: https://devweb2014.cis.strath.ac.uk/~ckb12185/CS317/NeighbourFood/business.php');                           
-        }
-        else if($type == "Charity")
-        {
-            session_start();
-            $_SESSION['login'] = "charity";
-            header('Location: https://devweb2014.cis.strath.ac.uk/~ckb12185/CS317/NeighbourFood/charity.php');              
-        }
-        // If the user tries to login with account credentials that aren't recognised
-        // the $_SESSION variable isn't set and they are taken back to the Login page.
-        else if (numrows < 1)
-        {
-            session_start();
-            $_SESSION['login'] = "";
-            header('Location: https://devweb2014.cis.strath.ac.uk/~ckb12185/CS317/NeighbourFood/login.php'); 
-            
-        }
-else // If nothing is entered into the login or password textbox.
-{
-    session_start();
-    $_SESSION['login'] = "";
-    header('Location: https://devweb2014.cis.strath.ac.uk/~ckb12185/CS317/NeighbourFood/login.php'); 
+    }
 }
 
-}			
+if ($password == $pass) {
+    // Check if its a Business or Charity that just logged in.
+    $sql = "SELECT Type FROM Client_Details WHERE Email = '$email'";
+    $result = mysqli_query($connection, $sql);
+
+    $numRows = mysqli_num_rows($result);
+
+    $type = "";
+    while ($row = mysqli_fetch_array($result)) {
+        $type = $row["Type"];
+    }
+
+    if ($type == "Business") {
+        session_start();
+        $_SESSION['login'] = "business";
+        header('Location: https://devweb2014.cis.strath.ac.uk/~ckb12185/CS317/NeighbourFood/business.php');
+    } else if ($type == "Charity") {
+        session_start();
+        $_SESSION['login'] = "charity";
+        header('Location: https://devweb2014.cis.strath.ac.uk/~ckb12185/CS317/NeighbourFood/charity.php');
+    }
+    // If the user tries to login with account credentials that aren't recognised
+    // the $_SESSION variable isn't set and they are taken back to the Login page.
+    else if (numrows < 1) {
+        session_start();
+        $_SESSION['login'] = "";
+        header('Location: https://devweb2014.cis.strath.ac.uk/~ckb12185/CS317/NeighbourFood/login.php');
+    }
+} else { // If nothing is entered into the login or password textbox.
+    session_start();
+    $_SESSION['login'] = "";
+    header('Location: https://devweb2014.cis.strath.ac.uk/~ckb12185/CS317/NeighbourFood/login.php');
+}
 ?>
