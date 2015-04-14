c49e3eb95bc2c6ff8ea2bc5f755a3573665e32ff
Smyth4 <ckb12185@uni.strath.ac.uk>
2015-04-14 22:11:10 +0100

    Update donate.php

--- Php-Files/donate.php
+++ Php-Files/donate.php
@@ -2,6 +2,8 @@
 
 session_start();
 
+include ("connection.php");
+
 ?>
 
 <!DOCTYPE html>
@@ -13,18 +15,86 @@ session_start();
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
+
 <body>
-    <form>
-        <input type="text" name="Name" placeholder="Item Description" />
+    <form method="POST" action="donate.php">
+        <input type="text" name="itemDescription" placeholder="Item Description" />
         <br />
-        <input type="text" name="Quantity" placeholder="Quantity" />
+        <input type="number" min="1" name="quantity" placeholder="Quantity" />
         <br />
-        <input type="email" name="Start" placeholder="Start Time" />
+        <input type="text" name="start" placeholder="Start Time" onfocus="this.placeholder = ''"/>
         <br />
-        <input type="password" name="End" placeholder="End Time" />
+        <input type="text" name="end" placeholder="End Time" onfocus="this.placeholder = ''"/>
         <br />   
+        <input type="submit" value="Donate">
     </form>
-    <button class="button">Donate</button>
     <script src="layout.js"></script>
 </body>
 </html>
+
+<?php
+
+$continue = false;
+
+if (isset($_POST['itemDescription'])) {
+    $itemDescription = $_POST['itemDescription'];
+
+    $itemDescriptionLength = strlen(trim($itemDescription));
+    if ($itemDescriptionLength >= 0 && $itemDescriptionLength <= 20) {
+        $continue = true;
+    } else {
+        $continue = false;
+    }
+}
+if (isset($_POST['quantity'])) {
+    $quantity = $_POST['quantity'];
+    
+    if ($quantity >= 1) {
+        $continue = true;
+    } else {
+        $continue = false;
+    }
+}
+if (isset($_POST['start'])) {
+    $start = $_POST['start'];
+
+    $startLength = strlen(trim($start));
+    if ($startLength > 0) {
+        $continue = true;
+    } else {
+        $continue = false;
+    }
+}
+if (isset($_POST['end'])) {
+    $end = $_POST['end'];
+
+    $endLength = strlen(trim($end));
+    if ($endLength > 0) {
+        $continue = true;
+    } else {
+        $continue = false;
+    }
+}
+
+// Prepared MySQL statement for adding a donation.
+if ($continue == true)
+{
+    if(isset($_SESSION['email']))
+    {
+        $email = $_SESSION['email'];
+    }   
+    
+    $sql = "INSERT INTO Food_Details ( Item,  Quantity,  Time_Start,  Claimed,  Business_Email, Time_End, ItemID)"
+                        .  "VALUES   (  ?,        ?,         ?,          0,           ?,           ?,      NULL )";
+    
+    $stmt = mysqli_stmt_init($connection);
+
+    if (mysqli_stmt_prepare($stmt, $sql))
+    {
+        mysqli_stmt_bind_param($stmt, 'sssss', $itemDescription, $quantity, $start, $email, $end);
+        mysqli_stmt_execute($stmt);
+    }  
+    header('Location: https://devweb2014.cis.strath.ac.uk/~ckb12185/CS317/NeighbourFood/donate.php');
+}
+
+?>
