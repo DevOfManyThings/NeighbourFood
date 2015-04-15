ec250110b02bc155bf5d8c38dc8c2e42dad637ee
Smyth4 <ckb12185@uni.strath.ac.uk>
2015-04-15 11:48:47 +0100

    Update business.php

--- Php-Files/business.php
+++ Php-Files/business.php
@@ -1,6 +1,8 @@
 <?php
+
 include ("connection.php");
-include ("checkBusinessLogin.php")
+include ("checkBusinessLogin.php");
+
 ?>
 
 
@@ -25,51 +27,66 @@ include ("checkBusinessLogin.php")
 
     <body>
         <!-- Navigate to donate page for business -->
-        <button onclick="navTo('donate.php')"> Donate Food </button>
-
-        <!-- Table displaying the business' current donations & their status -->
-        <table>
-            <thead>
-                <tr>
-                    <th>
-                        Name   
-                    </th>
-                    <th>
-                        Quantity  
-                    </th>
-                    <th>
-                        Start Time     
-                    </th>
-                    <th>
-                        End Time     
-                    </th>
-                    <th>
-                        Status     
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <!-- TODO Php: Make new <tr> for each item donated -->
-                <tr>
-                    <td>
-                        Donated Item i: Attribute "Name"  
-                    </td>
-                    <td>
-                        Donated Item i: Attribute "Quantity"
-                    </td>
-                    <td>
-                        Donated Item i: Attribute "Start Time" 
-                    </td>
-                    <td>
-                        Donated Item i: Attribute "End Time"
-                    </td>
-                    <td>
-                        Donated Item i: Attribute "Claimed/Not claimed"
-                    </td>
-                </tr>
-
-            </tbody>
-        </table>
+        <button onclick="navTo('donate.php')">Donate</button>
         <script src="layout.js"></script>
     </body>
 </html>
+
+<?php     
+
+    // PHP to show the donations.
+    $sql = "SELECT a.Item, a.Quantity, b.OrgName, a.Business_Email, a.Time_Start, a.Time_End, a.Claimed_By
+            FROM Food_Details a
+            INNER JOIN Client_Details b ON a.Business_Email = b.Email";
+
+    $result = mysqli_query($connection, $sql) or trigger_error("Query Failed: " . mysql_error());
+
+    $numRows = mysqli_num_rows($result);
+
+    if ($numRows > 0) 
+    {
+        echo "<p>Donations</p><table><tr><th>Item Description</th></tr><tr><th>Quantity</th><th><tr><th>Business Name</th><th><tr><th>Business Email</th><th><tr><th>Donation Date</th><th><tr><th>Donation Available Until</th><th><tr><th>Claimed By</th><th>";
+    
+    // Output data of each row.
+    while ($row = $result->fetch_assoc()) 
+    {
+        echo "<tr><td>" . $row["Item"] . " " . $row["Quantity"] . " " . $row["OrgName"] . " " . $row["Business_Email"] . " " . $row["Time_Start"] . " " . $row["Time_End"] . " " . $row["Claimed_By"] .  "</td></tr>";
+    }
+    echo "</table>";
+    } 
+    else 
+    {
+        echo "No Donations Have Been listed.";
+    }
+    
+     
+    // PHP to show what the business thats currently logged in has donated.
+    if(isset($_SESSION['email']))
+    {
+        $email = $_SESSION['email'];
+    } 
+    $sql = "SELECT a.Item, a.Quantity, b.OrgName, a.Business_Email, a.Time_Start, a.Time_End
+            FROM Food_Details a
+            INNER JOIN Client_Details b ON a.Business_Email = b.Email
+            WHERE a.Business_Email = '$email'";
+
+    $result = mysqli_query($connection, $sql) or trigger_error("Query Failed: " . mysql_error());
+
+    $numRows = mysqli_num_rows($result);
+
+    if ($numRows > 0) 
+    {
+        echo "<br><br>Donations Made By You.</br></br><table><tr><th>Item Description</th></tr><tr><th>Quantity</th><th><tr><th>Business Name</th><th><tr><th>Business Email</th><th><tr><th>Donation Date</th><th><tr><th>Donation Available Until</th><th>";
+    
+    // Output data of each row.
+    while ($row = $result->fetch_assoc()) 
+    {
+        echo "<tr><td>" . $row["Item"] . " " . $row["Quantity"] . " " . $row["OrgName"] . " " . $row["Business_Email"] . " " . $row["Time_Start"] . " " . $row["Time_End"] . "</td></tr>";
+    }
+    echo "</table>";
+    } 
+    else 
+    {
+        echo "You Have Made No Donations.";
+    }
+?>
