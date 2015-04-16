02938208ee98679c93fcaa65c1e8f414a6aaecea
Smyth4 <ckb12185@uni.strath.ac.uk>
2015-04-16 15:03:04 +0100

    Update charity.php

--- Php-Files/charity.php
+++ Php-Files/charity.php
@@ -24,7 +24,9 @@ include ("checkCharityLogin.php");
     </body>
 
     <body>
-        <?php      
+    <?php   
+    
+    // PHP to show the donations available.
     $sql = "SELECT a.Item, a.Quantity, b.OrgName, a.Business_Email, a.Time_Start, a.Time_End, a.Claimed_By
             FROM Food_Details a
             INNER JOIN Client_Details b ON a.Business_Email = b.Email";
@@ -67,7 +69,62 @@ include ("checkCharityLogin.php");
     } 
     else 
     {
-        echo "No Donations have been listed.";
+        echo "No Donations Have Been Listed.";
+    }
+    
+    
+    
+    
+    // PHP to show what the charity thats currently logged in has claimed.
+    if(isset($_SESSION['email']))
+    {
+        $email = $_SESSION['email'];
+    } 
+    
+    $sql = "SELECT a.Item, a.Quantity, b.OrgName, a.Business_Email, a.Time_Start, a.Time_End, a.Claimed_By
+            FROM Food_Details a
+            INNER JOIN Client_Details b ON a.Business_Email = b.Email
+            WHERE a.Claimed_By = '$email'";
+
+    $result = mysqli_query($connection, $sql) or trigger_error("Query Failed: " . mysql_error());
+
+    $numRows = mysqli_num_rows($result);
+
+    if ($numRows > 0) 
+    {
+        echo "Donations"
+        . "<table id=\"donations\">"
+                . "<thead>"
+                . "<tr>"
+                . "<th>Item Description</th>"
+                . "<th>Quantity</th>"
+                . "<th>Business Name</th>"
+                . "<th>Business Email</th>"
+                . "<th>Donated At</th>"
+                . "<th>Available Until</th>"
+                . "<th>Claimed By</th>"
+                . "</tr>"
+                . "</thead><tbody>";
+        
+    
+    // Output data of each row.
+    while ($row = $result->fetch_assoc()) 
+    {
+        echo "<tr>"
+        . "<td>" . $row["Item"] . "</td>"
+        . "<td>" . $row["Quantity"] . "</td>"
+        . "<td>" . $row["OrgName"] . "</td>"
+        . "<td>" . $row["Business_Email"] . "</td>" 
+        . "<td>" . $row["Time_Start"] . "</td>"
+        . "<td>" . $row["Time_End"] . "</td>" 
+        . "<td>" . $row["Claimed_By"] . "</td>"
+                . "</tr>";
+    }
+    echo "</tbody></table>";
+    } 
+    else 
+    {
+        echo "You have claimed no donations.";
     }
 ?>
         <script src="../layout.js"></script>
