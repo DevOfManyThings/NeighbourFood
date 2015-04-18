13b42354374607a32e2cc00814a03b886be3e617
Smyth4 <ckb12185@uni.strath.ac.uk>
2015-04-18 17:27:22 +0100

    Update business.php

--- Php-Files/business.php
+++ Php-Files/business.php
@@ -5,7 +5,6 @@ include ("checkBusinessLogin.php");
 
 ?>
 
-
 <!DOCTYPE html>
 <head>
     <title>NeighbourFood</title>
@@ -17,7 +16,7 @@ include ("checkBusinessLogin.php");
     <script src="navigate.js"></script>
 </head>
 
-    <body>   
+    <body>  
         <?php     
         
         echo "<header>"
@@ -29,7 +28,8 @@ include ("checkBusinessLogin.php");
         . "<input class=\"button\" type=\"submit\" value=\"Logout\">"
         . "</form>";
 
-    // PHP to show the donations.
+        
+    // PHP to show the donations available.
     $sql = "SELECT a.Item, a.Quantity, b.OrgName, a.Business_Email, a.Time_Start, a.Time_End, a.Claimed_By
             FROM Food_Details a
             INNER JOIN Client_Details b ON a.Business_Email = b.Email";
@@ -40,14 +40,14 @@ include ("checkBusinessLogin.php");
 
     if ($numRows > 0) 
     {
-        echo "<p>All Donations</p>"
+        echo "<p>Available Donations</p>"
         . "<table id=\"donations\">"
                 . "<thead>"
                 . "<tr>"
                 . "<th>Item Description</th>"
                 . "<th>Quantity</th>"
-                . "<th>Business Name</th>"
-                . "<th>Business Email</th>"
+                . "<th>Donated By</th>"
+                . "<th>Donator Contact</th>"
                 . "<th>Donated At</th>"
                 . "<th>Available Until</th>"
                 . "<th>Claimed By</th>"
@@ -58,23 +58,22 @@ include ("checkBusinessLogin.php");
     while ($row = $result->fetch_assoc()) 
     {
         echo "<tr>"
-            . "<td>" . $row["Item"] . "</td> " 
-            . "<td>" . $row["Quantity"] . "</td> " 
-            . "<td>" . $row["OrgName"] . "</td> " 
-            . "<td>" . $row["Business_Email"] . "</td> " 
-            . "<td>" . $row["Time_Start"] . "</td> "
-            . "<td>" . $row["Time_End"] . "</td> " 
-            . "<td>" . $row["Claimed_By"] . "</td> " 
-            . "</tr>";
+                . "<td>" . $row["Item"] . "</td> " 
+                . "<td>" . $row["Quantity"] . "</td> " 
+                . "<td>" . $row["OrgName"] . "</td> " 
+                . "<td>" . $row["Business_Email"] . "</td> " 
+                . "<td>" . $row["Time_Start"] . "</td> "
+                . "<td>" . $row["Time_End"] . "</td> " 
+                . "<td>" . $row["Claimed_By"] . "</td> " 
+                . "</tr>";
     }
     echo "</tbody></table>";
     } 
     else 
     {
-        echo "No Donations Have Been listed.";
+        echo "No Donations Have Been Listed.";
     }
     
-     
     // PHP to show what the business thats currently logged in has donated.
     if(isset($_SESSION['email']))
     {
@@ -92,7 +91,7 @@ include ("checkBusinessLogin.php");
     if ($numRows > 0) 
     {
         echo "<section>"
-        . "Donations Made By You."
+                . "Donations Made By You."
                 . "<table id=\"business\">"
                 . "<thead>"
                 . "<tr>"
@@ -108,11 +107,11 @@ include ("checkBusinessLogin.php");
     while ($row = $result->fetch_assoc()) 
     {
         echo "<tr>"
-        . "<td>" . $row["Item"] . "</td>"
-                . "<td>" . $row["Quantity"] . "</td>"
-                . "<td>" . $row["Time_Start"] . "</td>"
-                . "<td>" . $row["Time_End"] . "</td>"
-                . "</tr>";
+            . "<td>" . $row["Item"] . "</td>"
+            . "<td>" . $row["Quantity"] . "</td>"
+            . "<td>" . $row["Time_Start"] . "</td>"
+            . "<td>" . $row["Time_End"] . "</td>"
+            . "</tr>";
     }
     echo "</tbody></table>";
     } 
@@ -124,4 +123,3 @@ include ("checkBusinessLogin.php");
         <script src="../layout.js"></script>
     </body>
 </html>
-
