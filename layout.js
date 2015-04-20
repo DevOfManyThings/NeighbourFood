b2ecc3b9f011afa281209760b64a12be4f605935
RickyProvan <richardprovan.glasgow@gmail.com>
2015-04-20 10:43:24 +0100

    Style changes: Added media query to change root font size if screen if slim/ flipped

--- layout.js
+++ layout.js
@@ -1,6 +1,9 @@
 /*jslint node: true, browser: true */
 "use strict";
-/* Function to deal with height/ vertical spacing issues in css, and resize as needed to fit each screen. */
+/* Function to deal with height/ vertical spacing issues in css, and resize as needed to fit each screen. 
+ * Used alongside media query in css3 to return less diverse results of how elements are displayed
+ * on devices with different screen proportions. Also allows for screen-rotation adjustments to keep
+ * elements looking right. */
 function resize() {
     /* Resize element height to fit screen
      *  L = unit for one percent of screen height
@@ -21,29 +24,22 @@ function resize() {
                 element.tagName !== "BODY") {
             
             if (element.id === "icon") {
-                //20
                 element.style.height = String(15 * L) + "px";
                 element.style.marginTop = String(3 * L) + "px";
                 element.style.marginBottom = String(2 * L) + "px";
             }
             if (element.id === "mainHeading") {
-                //15
                 element.style.height = String(10 * L) + "px";
                 element.style.marginTop = String(L) + "px";
                 element.style.marginBottom = String(2 * L) + "px";
                 element.style.paddingTop = String(2 * L) + "px";
             }
             if (element.tagName === "HR") {
-           
                 element.style.marginTop = String(10 * L) + "px";
                 element.style.marginBottom = String(5 * L) + "px";
-
             }
             if (element.tagName === "INPUT" || element.tagName === "BUTTON"){
-                //10
                 if(element.getAttribute("type") !== "radio") {
-                    
-               
                     element.style.height = String(7 * L) + "px";
                     element.style.marginTop = String(2 * L) + "px";
                     element.style.marginBottom = String(1 * L) + "px";
