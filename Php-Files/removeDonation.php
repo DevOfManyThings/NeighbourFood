419c6eb93214e0745663b3da29e72405185ca407
Smyth4 <ckb12185@uni.strath.ac.uk>
2015-04-21 12:26:21 +0100

    Create removeDonation.php

---
+++ Php-Files/removeDonation.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+
+include ("connection.php");
+
+$ItemID = $_POST['id'];
+
+$sql = "DELETE FROM Food_Details
+        WHERE ItemID = '$ItemID'";
+                  
+mysqli_query($connection, $sql);
+
+header('Location: myDonations.php');
+
+?>
