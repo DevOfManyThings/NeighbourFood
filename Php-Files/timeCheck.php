bcdb41c78a55d002ee54d9516e1fdc7faf0baf43
Smyth4 <ckb12185@uni.strath.ac.uk>
2015-04-25 13:14:02 +0100

    Update timeCheck.php

--- Php-Files/timeCheck.php
+++ Php-Files/timeCheck.php
@@ -6,8 +6,9 @@ include ("connection.php");
 //echo "The current server timezone is: " . $timezone;
 
 $currentTime = date('H:i', time());
+$currentDate = date('Y-m-d');
 
-$sql = "SELECT DATE_FORMAT(Time_End, '%H:%i') AS Time_End
+$sql = "SELECT DATE_FORMAT(Time_End, '%H:%i') AS Time_End, Date_Donated
                FROM Food_Details";
 
 $result = mysqli_query($connection, $sql) or trigger_error("Query Failed: " . mysql_error());
@@ -17,8 +18,9 @@ $numRows = mysqli_num_rows($result);
 if ($numRows > 0) {
     while ($row = $result->fetch_assoc()) {
         $timeEnd = $row["Time_End"];
-
-        if ($currentTime >= $timeEnd) {
+        $date = $row["Date_Donated"];
+        
+        if ($currentDate >= $date && $currentTime >= $timeEnd) {
             $sql = "DELETE FROM Food_Details
                 WHERE Time_End = '$timeEnd'";
 
